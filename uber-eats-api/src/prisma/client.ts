64b2f7146465d8a3b382a9fe4191593eddf7e2e7
bocarformation <bocsco3@gmail.com>
2026-01-12 12:22:02 +0100

    Fix prisma version with adapter-pg

--- uber-eats-api/src/prisma/client.ts
+++ uber-eats-api/src/prisma/client.ts
@@ -1,3 +1,12 @@
 import {PrismaClient} from "@prisma/client";
+import "dotenv/config";
+import {Pool} from "pg";
+import { PrismaPg } from "@prisma/adapter-pg"
 
-export const prisma = new PrismaClient({});
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL
+})
+
+export const prisma = new PrismaClient({
+    adapter: new PrismaPg(pool)
+});
