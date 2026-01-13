5294b6d4a3411a1ea6a8cf32c147adaeb7988d02
bocarformation <bocsco3@gmail.com>
2026-01-13 16:50:48 +0100

    Fix multer new images

--- uber-eats-api/src/middlewares/index.ts
+++ uber-eats-api/src/middlewares/index.ts
@@ -1,3 +1,7 @@
 export * from "./json-api-response.middleware";
 export * from "./authentication.middleware";
 export * from "./upload.middleware";
+
+import fs from "fs";
+
+fs.unlink("./images/"+restaurant.coverImages[index])
